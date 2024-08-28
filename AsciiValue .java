69cecf9270ae8f29faf7453e2ce3fd409587fdee
madhu999-svg <thanneerumadhuravani@gmail.com>
2024-08-28 10:36:11 +0530

    Add files via upload

---
+++ AsciiValue .java
@@ -0,0 +1,24 @@
+public class AsciiValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			/*	char ch='A';
+				int asciivalue=ch;
+				System.out.println("Ascii value of "+ch+" is:"+asciivalue);
+				// TODO Auto-generated method stub
+
+				char ch='a';
+				int asciivalue=ch;
+				System.out.println("Ascii value of "+ch+" is:"+asciivalue);
+				// TODO Auto-generated method stub*/
+		
+		char ch='6';
+		int asciivalue=ch;
+		System.out.println("Ascii value of "+ch+" is:"+asciivalue);
+		// TODO Auto-generated method stub
+
+
+			}
+
+}
